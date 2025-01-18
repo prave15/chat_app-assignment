2296a6aeace109525a998b89817049551e79b069
P1999-birth <praveengurjar1947@gmail.com>
2025-01-18 23:28:47 +0530

    updated settings.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne chat_app.asgi:application -b 0.0.0.0 -p $PORT
+web: daphne -b 0.0.0.0 -p $PORT chat_app.asgi:application
--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
+
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
